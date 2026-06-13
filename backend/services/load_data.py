b4586c358bb9d806b87864cd15bb6a44d62ddcd5
palakchiraniya03 <palakschiraniya@gmail.com>
2026-06-13 23:19:03 +0530

    integarte dbscan backend

--- backend/services/load_data.py
+++ backend/services/load_data.py
@@ -4,12 +4,8 @@ def load_crime_data():
 
     df = pd.read_csv(
         "data/crime_dataset.csv",
-        low_memory=False
-    )
-
-    df = df.sample(
-        1000,
-        random_state=42
+        low_memory=False,
+        nrows=5000
     )
 
     return df
